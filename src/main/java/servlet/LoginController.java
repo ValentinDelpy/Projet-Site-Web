280422b014c464ac30ef46bb1f3ca9242e087862
Mark <Mark@DESKTOP-QJT29SN.univ-jfc.fr>
2019-03-26 12:59:47 +0100

    la vie va

--- src/main/java/servlet/LoginController.java
+++ src/main/java/servlet/LoginController.java
@@ -22,11 +22,9 @@ import model.DAO;
 import model.DiscountCode;
 import model.PurchaseOrder;
 
-/**
- *
- * @author DGX
- */
-public class LoginController extends HttpServlet{
+public class LoginController extends HttpServlet {
+    
+    
      protected void processRequest(HttpServletRequest request, HttpServletResponse response)
 		throws ServletException, IOException, SQLException {
 		// Quelle action a appelé cette servlet ?
@@ -59,24 +57,32 @@ public class LoginController extends HttpServlet{
 			jspView = "WEB-INF/customer.jsp";
 		}
                 
-//                else if ("admin".equals(userName)){
-//                    jspView = "WEB-INF/admin.jsp";
-//                }
+                else if ("admin".equals(userName)){
+                    jspView = "WEB-INF/admin.jsp";
+                }
 		// On va vers la page choisie
 		request.getRequestDispatcher(jspView).forward(request, response);
 
 	}
-     
 
-     
-     private void checkLogin(HttpServletRequest request) throws SQLException {
+	/**
+	 * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+	 *
+	 * @param request servlet request
+	 * @param response servlet response
+	 * @throws ServletException if a servlet-specific error occurs
+	 * @throws IOException if an I/O error occurs
+	 */
+	private void checkLogin(HttpServletRequest request) {
+            
                 // on va créer un DAO pour pouvoir intéragir avec la bdd
                 DAO dao = new DAO();
-
-		// Les paramètres transmis dans la requête
                 
+                
+		// Les paramètres transmis dans la requête
 		String loginParam = request.getParameter("loginParam");
 		String passwordParam = request.getParameter("passwordParam");
+                
                 // on va d'abord vérifier si c'est un admin qui tente de se connecter
                 if ((loginParam.equals("admin@admin") && (passwordParam.equals("admin")))) {
 			// On a trouvé la combinaison login / password
@@ -84,83 +90,6 @@ public class LoginController extends HttpServlet{
 			HttpSession session = request.getSession(true); // démarre la session
 			session.setAttribute("userName", "admin");
 		}
-                else if(!"".equals(loginParam) && !"".equals(passwordParam)){
-                   Customer c = dao.findCustomerID(loginParam);
-               // Le login/password défini dans les propiétés du customer
-                   String login = c.getEmail();
-                   String password = c.getPassword();
-                   String userName = c.getName();
-                   String phone = c.getPhone();
-                   String address = c.getAddressline1();
-                   
-                    // si le mail et le mdp correspondent alors on peut se connecter
-                    if ((login.equals(loginParam) && (password.equals(passwordParam)))) {
-			// On a trouvé la combinaison login / password
-			// On stocke l'information dans la session
-			HttpSession session = request.getSession(true); // démarre la session
-			session.setAttribute("userName", userName);
-                        session.setAttribute("userEmail", login);
-                        session.setAttribute("userPassword", password);
-                        session.setAttribute("userAddress", address);
-                        session.setAttribute("userPhone", phone);
-                        session.setAttribute("commandes", dao.customerCommandes(c));  
-                        ArrayList<String> des = dao.allProduct();
-                        request.setAttribute("listeProduits", des);
-                        Double solde = dao.soldeClient(Integer.parseInt(password));
-                        session.setAttribute("solde", solde);
-                        session.setAttribute("codes", viewCodes(request));
-                    }
-                    else if (login.equals("nodata")){
-                        request.setAttribute("errorMessage", "Login/Password incorrect");
-                    }
-                    else if ("".equals(loginParam) || "".equals(passwordParam)){ // On positionne un message d'erreur pour l'afficher dans la JSP
-                            request.setAttribute("errorMessage", "Login/Password incorrect");
-                    }
-                    
-                    
-                    else{
-                        request.setAttribute("errorMessage", "Login/Password incorrect");
-                    }
-                 }
                 
-                
-                // on créé un customer à partir de la BDD
-                
-	}
-
-	private void doLogout(HttpServletRequest request) {
-		// On termine la session
-		HttpSession session = request.getSession(false);
-		if (session != null) {
-			session.invalidate();
-		}
-	}
-
-	private String findUserInSession(HttpServletRequest request) {
-		HttpSession session = request.getSession(false);
-		return (session == null) ? null : (String) session.getAttribute("userName");
-	}
-        
-         public List<PurchaseOrder> viewCommandes(HttpServletRequest request) throws SQLException {
-            List<PurchaseOrder> result = new LinkedList<>();
-            DAO dao= new DAO();
-            HttpSession session = request.getSession();
-            String password = ((String)session.getAttribute("userPassword"));
-            Customer c = new Customer();
-            c.setPassword(password);
-            result = dao.customerCommandes(c);                    
-            return result;
-         }
-         
-        public List<DiscountCode> viewCodes(HttpServletRequest request) throws SQLException {
-            List<DiscountCode> result = new LinkedList<>();
-            DAO dao= new DAO();
-            HttpSession session = request.getSession();
-            String password = ((String)session.getAttribute("userPassword"));
-            Customer c = new Customer();
-            c.setPassword(password);
-            result = dao.customerCodes(c);         
-            
-            return result;
-    }
-}
+        }
+        }
